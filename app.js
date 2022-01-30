c56253bce1e9e1775dfd2d2331daf2035210cf83
Jon Michael <jmvlu27@hotmail.com>
2022-01-29 19:02:47 -0500

    add prevGuesses property and get Guess method

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  },
+
+  getGuess: function(guessedNum) {
+    let guess = Number(window.prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`, " ")); 
+    let num = parseInt(guess)
+    if (num >= this.smallestNum && num <= this.biggestNum) {
+      return num
+    } else {
+      return NaN
+    }
   }
 }
+// console.log(game.getGuess())
+
+console.log(game.getGuess())
