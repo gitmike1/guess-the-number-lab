238c943bd81ba75e992ac60e1de5c45aeb35b0f2
Jon Michael <jmvlu27@hotmail.com>
2022-01-29 21:49:01 -0500

    solve step 4

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    let num = null
+      while (num !== this.secretNum) {
+        let num = this.getGuess()
+      this.prevGuesses.push(num)
+      console.log(game.prevGuesses)
+      }
   },
 
-  getGuess: function(guessedNum) {
-    let guess = Number(window.prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`, " ")); 
-    let num = parseInt(guess)
-    if (num >= this.smallestNum && num <= this.biggestNum) {
-      return num
-    } else {
-      return NaN
+  getGuess: function() {
+    let guess = parseInt(prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`,)); 
+    //console.log(typeof guess)
+    //let num = parseInt(guess)
+    //console.log(typeof num)
+    while (guess < this.smallestNum || guess > this.biggestNum) {
+      guess = parseInt(prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`,));
     }
+    //deal with NaN from non-number guess
+  return guess
+  console.log("validInput")
   }
 }
-// console.log(game.getGuess())
-
-console.log(game.getGuess())
+ //console.log(game.getGuess())
+game.play()
